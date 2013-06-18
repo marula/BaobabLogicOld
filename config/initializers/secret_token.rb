d0bc3194638b700f80db25de10e7abee8dcd000f
Chris-M <christophermmiller101@gmail.com>
2013-06-18 15:45:36 +0200

    Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaobabLogic::Application.config.secret_key_base = '47aca7b259ef9b4a8fe8e70c7f64b644b4317f4a6398c7ae5fc05c738bc67ece2b3d29a7779ca3c69b29d8c8478822544bb22ea824f3ba90838f7913d603e647'
