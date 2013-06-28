dbf681b7663502df23f07357469b6b5b707c1986
alex <alex@baobablogic.com>
2013-06-28 13:37:38 +0200

    Working with dup

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,7 +25,9 @@ class BlogsController < ApplicationController
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
+    
+    @blog.user_id = current_user.id
+    
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog post was successfully created.' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BaobabLogic::Application.routes.draw do
   #get "/blog" => "blogs#index"
 
+  root :to => 'blogs#index'
+
   resources :blogs
 
   resources :projects
