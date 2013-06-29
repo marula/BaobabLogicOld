d1a3e960917e8f379bd37f472958c33febb2bf64
Rick Kleinhans <rick@baobablogic.com>
2013-06-29 12:59:06 +0200

    Initial setup for user login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+	end
+
+	def create
+	  user = User.authenticate(params[:email], params[:password])
+	  if user
+	    session[:user_id] = user.id
+	    if session[:return_to]
+	    	redirect_to session[:return_to], :notice => "Logged in!"
+	    else
+	    	redirect_to root_url, :notice => "Logged in!"
+	  	end
+	  else
+	    flash.now.alert = "Invalid email or password"
+	    render "new"
+	  end
+	end
+
+	def destroy
+	  session[:user_id] = nil
+	  redirect_to root_url, :notice => "Logged out!"
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 BaobabLogic::Application.routes.draw do
+	get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+
  	get "/blog" => "blogs#index"
 
-  resources :blogs
-  resources :projects
-  resources :users
+  resources :blogs, :projects, :users, :sessions
 
 	root :to => 'pages#home'
 end
